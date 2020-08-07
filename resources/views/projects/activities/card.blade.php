89dea08bda18e62a3c4e2fa4b27cd3d2df2b010d
Luteranya Nyange <dieume.dev@gmail.com>
2020-08-07 22:39:47 +0300

    descriptive project activity on the ui

--- resources/views/projects/activities/card.blade.php
+++ resources/views/projects/activities/card.blade.php
@@ -1,10 +1,12 @@
 <div class="card mt-4">
     <ul class="text-xs">
-        @foreach ($project->activities as $activity)
+        @forelse ($project->activities as $activity)
         <li class="{{ $loop->last ? '' : 'mb-1' }}">
             @include("projects.activities.{$activity->description}")
             <span class="text-gray-500 ml-2">{{ $activity->created_at->diffForHumans() }}</span>
         </li>
-        @endforeach
+        @empty
+        <h3>No activity</h3>
+        @endforelse
     </ul>
 </div>
